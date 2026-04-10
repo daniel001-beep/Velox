9bff796f14dc2712d226ac1d32b1d8473d213e23
idowu daniel <idowuisdaniel1@gmail.com>
2026-04-10 21:30:31 +0100

    Final Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // MUST be here for that script in your screenshot to work
-  basePath: '/Redstore', 
   images: {
     unoptimized: true, 
   },
